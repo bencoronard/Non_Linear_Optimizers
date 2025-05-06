91f66dc858f0c45b94323abbb21483429358747a
Ben <128577990+bencoronard@users.noreply.github.com>
2025-05-06 16:05:37 +0700

    Create test modules

--- src/test/java/dev/hireben/demo/rest/scheduler/SchedulerApplicationTests.java
+++ src/test/java/dev/hireben/demo/rest/scheduler/SchedulerApplicationTests.java
@@ -1,4 +1,4 @@
-package com.hengleasing.portal.scheduler;
+package dev.hireben.demo.rest.scheduler;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -8,8 +8,8 @@ import org.springframework.context.annotation.Import;
 @SpringBootTest
 class SchedulerApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+  @Test
+  void contextLoads() {
+  }
 
 }
---
+++ src/test/java/dev/hireben/demo/rest/scheduler/TestSchedulerApplication.java
@@ -0,0 +1,11 @@
+package dev.hireben.demo.rest.scheduler;
+
+import org.springframework.boot.SpringApplication;
+
+public class TestSchedulerApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.from(SchedulerApplication::main).with(TestcontainersConfiguration.class).run(args);
+  }
+
+}
