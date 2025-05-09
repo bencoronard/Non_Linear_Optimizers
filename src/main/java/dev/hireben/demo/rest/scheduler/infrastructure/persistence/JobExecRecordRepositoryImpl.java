52456960c07cdb00720948970cad0214bbb46009
Ben <128577990+bencoronard@users.noreply.github.com>
2025-05-09 11:55:53 +0700

    Create JobExecRecordRepositoryImpl.java

---
+++ src/main/java/dev/hireben/demo/rest/scheduler/infrastructure/persistence/JobExecRecordRepositoryImpl.java
@@ -0,0 +1,30 @@
+package dev.hireben.demo.rest.scheduler.infrastructure.persistence;
+
+import org.springframework.stereotype.Repository;
+
+import dev.hireben.demo.rest.scheduler.domain.entity.JobExecRecord;
+import dev.hireben.demo.rest.scheduler.domain.repository.JobExecRecordRepository;
+import dev.hireben.demo.rest.scheduler.infrastructure.persistence.jpa.mapper.JobExecRecordEntityMapper;
+import dev.hireben.demo.rest.scheduler.infrastructure.persistence.jpa.repository.JobExecRecordJpaRepository;
+import lombok.RequiredArgsConstructor;
+
+@Repository
+@RequiredArgsConstructor
+public class JobExecRecordRepositoryImpl implements JobExecRecordRepository {
+
+  // ---------------------------------------------------------------------------//
+  // Fields
+  // ---------------------------------------------------------------------------//
+
+  private final JobExecRecordJpaRepository repository;
+
+  // ---------------------------------------------------------------------------//
+  // Methods
+  // ---------------------------------------------------------------------------//
+
+  @Override
+  public void save(JobExecRecord record) {
+    repository.save(JobExecRecordEntityMapper.toEntity(record));
+  }
+
+}
